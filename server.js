38dcdbb7e37a9e5b3fd29f90dddde37eb44b3f89
ndebraska <castron1@gmatc.matc.edu>
2021-10-11 15:20:20 -0500

    Set up Project 1

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 //API endpoints
 app.get('/', (req,res) =>
-    res.send('http get request sent to root api endpoint - P1')
+    res.send('Project 1 - http get request sent to root api endpoint')
 );
 
 //Connection listener
-app.listen(3000, () => console.log('Express server running on port 3000 - P1'));
+app.listen(3000, () => console.log('Project 1 - Express server running on port 3000'));
